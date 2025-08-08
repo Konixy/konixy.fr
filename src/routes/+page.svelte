71c95ee0cbc0c74d8daca6e10d36705ea1f3c5ab
Anatole Dufour <konixy.p@gmail.com>
2025-08-08 14:31:18 +0200

    Rewriting the project from scratch

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import Title from '$lib/components/title.svelte';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center gap-10">
+	<Title />
+	<p class="text-foreground/60 text-center text-xl">Développeur web fullstack</p>
+</div>
